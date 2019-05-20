12d035e2b84d0a1fc2883fb859c059698388cd03
Mike <migueljrojas@gmail.com>
2019-05-20 15:21:27 -0300

    Finales Seat

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -10,6 +10,39 @@ var Header = function() {
         header.toggleClass('-open');
         body.toggleClass('-hideOverflow');
     });
+
+    var productCategory = $('[data-category]');
+    var productFilters = $('[data-filter]');
+    var filterSelector = $('.home__products__filter-selector');
+    var filterList = $('.home__products__filters');
+
+    filterSelector.on('click', function(e){
+        e.preventDefault();
+        filterList.toggleClass('-open');
+    });
+
+    productFilters.on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var targetFilter = $this.data('filter');
+
+        filterList.removeClass('-open');
+
+        if (targetFilter === 'all') {
+            productCategory.removeClass('-hidden');   
+            productFilters.removeClass('-active');
+            $('[data-filter="'+ targetFilter +'"]').addClass('-active');
+            return;
+        }
+
+        productCategory.addClass('-hidden');
+        productFilters.removeClass('-active');        
+        $('[data-filter="'+ targetFilter +'"]').addClass('-active');
+
+        productCategory.filter(function() {
+            return $(this).data('category') === targetFilter;
+        }).removeClass('-hidden');
+    });
 };
 
 module.exports = Header;
